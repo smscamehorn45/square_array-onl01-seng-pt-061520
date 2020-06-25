c73a3aba46b8774742f1af770a9ca3015f8c6262
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-25 04:49:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,9 +5,3 @@ def square_array(array)
 end
 
 arr = [1, 2, 3, 4, 5]
-square_array(arr)
-# //arr.each { |a| print a -= 10, " " }
-# # prints: -9 -8 -7 -6 -5
-#=> [1, 2, 3, 4, 5]
-
-# basket.each do |apple|
